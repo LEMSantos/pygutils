985c785e33f7945b9082c9f01fe2ba5974a0b96e
LEMSantos <lu.casmou.ra177833@gmail.com>
2023-09-19 07:58:14 -0300

    docs: adiciona mais informações para a descrição do projeto

--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 DESCRIPTION = "Package with utility functions and classes to work with Pygame."
 
 setup(
     name="pygutils",
     version=VERSION,
+    license="MIT",
     author="Lucas Eliaquim",
     author_email="<lemsantos.dev@gmail.com>",
     description=DESCRIPTION,
@@ -21,6 +22,10 @@ setup(
     packages=find_packages(),
     install_requires=["pygame"],
     keywords=["python", "pygame", "utility", "helpers"],
+    project_urls={
+        "Bug Tracker": "https://github.com/LEMSantos/pygutils/issues",
+        "Source": "https://github.com/LEMSantos/pygutils",
+    },
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
@@ -29,4 +34,5 @@ setup(
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
     ],
+    python_requires=">=3.10",
 )
