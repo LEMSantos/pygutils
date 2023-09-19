f811bd3213133233afd67395c5d36e47783807cf
LEMSantos <lu.casmou.ra177833@gmail.com>
2023-09-19 11:28:00 -0300

    refactor: integra o current_ms_time a classe do TImer

--- pygutils/timer/timer.py
+++ pygutils/timer/timer.py
@@ -1,6 +1,5 @@
+import time
 from typing import Callable
-
-from .utils import current_ms_time
 
 
 class Timer:
@@ -13,18 +12,25 @@ class Timer:
         self.callback = callback
         self.start_time = None
 
+    @staticmethod
+    def current_ms_time() -> int:
+        return round(time.time() * 1000)
+
     @property
     def active(self) -> bool:
         return self.start_time is not None
 
     def activate(self) -> None:
-        self.start_time = current_ms_time()
+        self.start_time = Timer.current_ms_time()
 
     def deactivate(self) -> None:
         self.start_time = None
 
     def update(self) -> None:
-        if self.active and (current_ms_time() - self.start_time) >= self.duration_ms:
+        if (
+            self.active
+            and (Timer.current_ms_time() - self.start_time) >= self.duration_ms
+        ):
             if self.callback is not None:
                 self.callback()
 
--- pygutils/timer/utils.py
+++
@@ -1,5 +0,0 @@
-import time
-
-
-def current_ms_time() -> int:
-    return round(time.time() * 1000)
