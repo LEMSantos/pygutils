90c2338cb688f868db2f9790d6aaa3e99dbc586c
LEMSantos <lu.casmou.ra177833@gmail.com>
2023-09-21 23:46:15 -0300

    docs: ajusta a exibição da documentação no PyPI

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
+    long_description = "\n" + "\n".join(fh.readlines()[2:])
 
-VERSION = "0.0.14"
+VERSION = "0.0.15"
 DESCRIPTION = "Package with utility functions and classes to work with Pygame."
 
 setup(
